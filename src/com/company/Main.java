7768f827325f42e7148bbf0e63d51f4cf69d68c0
Nulllink <47364651+Nulllink@users.noreply.github.com>
2021-09-21 10:36:01 +0300

    1 commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+import java.util.Scanner;
+
+public class Main {
+	int name;
+	public static void main(String[] args) {
+		int age = 40;
+		int money = 400;
+		if(!(age > 21) || !(money > 500)){
+			System.out.println("Welcome");//TODO fix bug
+		}
+
+	}
+	public void func(){
+
+	}
+}
